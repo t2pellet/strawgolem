36f2da413ede79485af336f67b9e8a77ed836e82
Francesco Di Gennaro <fradige95@gmail.com>
2018-08-23 19:22:10 +0200

    Update version

--- src/main/java/nivoridocs/strawgolem/Strawgolem.java
+++ src/main/java/nivoridocs/strawgolem/Strawgolem.java
@@ -16,7 +16,7 @@ import org.apache.logging.log4j.Logger;
 public class Strawgolem {
     public static final String MODID = "strawgolem";
     public static final String NAME = "Straw Golem";
-    public static final String VERSION = "1.0a";
+    public static final String VERSION = "1.1";
 
     @SidedProxy(
     		serverSide = "nivoridocs.strawgolem.proxy.ServerProxy",
