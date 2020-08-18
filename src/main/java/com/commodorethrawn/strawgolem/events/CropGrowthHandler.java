a3e23a75b69c52953648d49fa1966d9c373bf8a6
daPoseidonGuy <pelletiertenzin2000@gmail.com>
2020-08-18 13:12:25 -0400

    Improved crop growth handling / harvest detection

--- src/main/java/com/commodorethrawn/strawgolem/events/CropGrowthHandler.java
+++ src/main/java/com/commodorethrawn/strawgolem/events/CropGrowthHandler.java
@@ -15,15 +15,14 @@ import net.minecraftforge.event.entity.player.BonemealEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.thread.EffectiveSide;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
+import java.util.PriorityQueue;
 
 /**
  * Handles how golems tend to harvest crops
- * <p>
  * Every time a crop grows, it checks for a golem nearby to harvest. If none is found it is added to a queue
  * Every 100 ticks, it goes through the queue checking for golems to harvest, skipping through already harvested blocks
  * and breaking when it cannot find a golem to harvest
@@ -31,9 +30,11 @@ import java.util.Queue;
 @Mod.EventBusSubscriber(modid = Strawgolem.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class CropGrowthHandler {
 
-    public static Queue<CropQueueEntry> cropQueue = new LinkedList<>();
-    public static StrawgolemSaveData data;
-    private static int tick = 0;
+    public static final PriorityQueue<CropQueueEntry> queue = new PriorityQueue<>();
+    private static final int HARVEST_DELAY = 100;
+    static StrawgolemSaveData data;
+
+    private CropGrowthHandler() {}
 
     @SubscribeEvent
     public static void serverStart(FMLServerStartingEvent event) {
@@ -41,72 +42,57 @@ public class CropGrowthHandler {
         data = StrawgolemSaveData.get(event.getServer().getWorld(DimensionType.OVERWORLD));
     }
 
+    private static int ticks = 0;
     @SubscribeEvent
-    public static void onServerTick(TickEvent.ServerTickEvent event) {
-        if (event.phase == TickEvent.Phase.END) {
-            if (tick % 200 == 0) {
-                int iteration = 0;
-                while (!cropQueue.isEmpty() && iteration < cropQueue.size()) {
-                    if (!isFullyGrown(cropQueue.peek())) {
-                        cropQueue.remove();
-                        data.markDirty();
-                        continue;
-                    }
-                    EntityStrawGolem golem = getCropGolem(cropQueue.peek());
-                    if (golem != null) {
-                        golem.setHarvesting(cropQueue.remove().getPos());
-                    } else cropQueue.add(cropQueue.remove());
-                    data.markDirty();
-                    ++iteration;
-                }
+    public static void tick(TickEvent.ServerTickEvent event) {
+        if (event.phase == TickEvent.Phase.END && EffectiveSide.get().isServer()) {
+            while (!queue.isEmpty() && ticks == queue.peek().tick) {
+                queue.remove().execute();
+                data.markDirty();
             }
-            ++tick;
+            ++ticks;
         }
     }
 
     @SubscribeEvent
     public static void onCropGrowth(BlockEvent.CropGrowEvent.Post event) {
-        CropQueueEntry entry = new CropQueueEntry(event.getPos(), event.getWorld());
-        if (!event.getWorld().isRemote() && isFullyGrown(entry)) {
-            EntityStrawGolem golem = getCropGolem(entry);
+        if (!event.getWorld().isRemote() && isFullyGrown(event.getWorld(), event.getPos())) {
+            EntityStrawGolem golem = getCropGolem(event.getWorld(), event.getPos());
             if (golem != null) {
-                golem.setHarvesting(entry.getPos());
+                golem.setHarvesting(event.getPos());
             } else {
-                cropQueue.add(new CropQueueEntry(event.getPos(), event.getWorld()));
-                data.markDirty();
+                scheduleCrop(event.getWorld(), event.getPos(), 12);
             }
         }
     }
 
     @SubscribeEvent
     public static void onCropGrowth(BonemealEvent event) {
-        CropQueueEntry entry = new CropQueueEntry(event.getPos(), event.getWorld());
-        if (!event.getWorld().isRemote() && isNearlyGrown(entry)) {
-            EntityStrawGolem golem = getCropGolem(entry);
+        if (!event.getWorld().isRemote() && isNearlyGrown(event.getWorld(), event.getPos())) {
+            EntityStrawGolem golem = getCropGolem(event.getWorld(), event.getPos());
             if (golem != null) {
-                golem.setHarvesting(entry.getPos());
+                golem.setHarvesting(event.getPos());
             } else {
-                cropQueue.add(new CropQueueEntry(event.getPos(), event.getWorld()));
-                data.markDirty();
+                scheduleCrop(event.getWorld(), event.getPos(), 12);
             }
         }
     }
 
     /**
      * Returns the first golem nearby that has path to the crop, or null if there are none
-     *
-     * @param crop the cropqueueentry
+     * @param world the world
+     * @param pos the position
      * @return applicable golem or null if none apply
      */
-    private static EntityStrawGolem getCropGolem(CropQueueEntry crop) {
-        AxisAlignedBB golemAABB = new AxisAlignedBB(crop.pos).grow(
+    private static EntityStrawGolem getCropGolem(IWorld world, BlockPos pos) {
+        AxisAlignedBB golemAABB = new AxisAlignedBB(pos).grow(
                 ConfigHelper.getSearchRangeHorizontal(),
                 ConfigHelper.getSearchRangeVertical(),
                 ConfigHelper.getSearchRangeHorizontal());
-        List<EntityStrawGolem> golemList = crop.world.getEntitiesWithinAABB(EntityStrawGolem.class, golemAABB);
+        List<EntityStrawGolem> golemList = world.getEntitiesWithinAABB(EntityStrawGolem.class, golemAABB);
         for (EntityStrawGolem golem : golemList) {
             if (golem.getHarvestPos().equals(BlockPos.ZERO)
-                    && golem.canSeeBlock(crop.world, crop.pos)
+                    && golem.canSeeBlock(world, pos)
                     && golem.isHandEmpty()) {
                 return golem;
             }
@@ -117,11 +103,12 @@ public class CropGrowthHandler {
     /**
      * Returns true if the crop is applicable and fully grown, false otherwise
      *
-     * @param entry the cropqueueentry
+     * @param world the world
+     * @param pos the position
      * @return whether the block is fully grown
      */
-    private static boolean isFullyGrown(CropQueueEntry entry) {
-        BlockState state = entry.getWorld().getBlockState(entry.getPos());
+    private static boolean isFullyGrown(IWorld world, BlockPos pos) {
+        BlockState state = world.getBlockState(pos);
         if (ConfigHelper.blockHarvestAllowed(state.getBlock())) {
             if (state.getBlock() instanceof CropsBlock) {
                 CropsBlock crop = (CropsBlock) state.getBlock();
@@ -143,11 +130,12 @@ public class CropGrowthHandler {
      * Returns true if the crop is applicable and almost fully grown, false otherwise
      * Used for bone meal event
      *
-     * @param entry the CropQueueEntry
+     * @param world the world
+     * @param pos the position
      * @return whether the crop is nearly fully grown
      */
-    private static boolean isNearlyGrown(CropQueueEntry entry) {
-        BlockState state = entry.getWorld().getBlockState(entry.getPos());
+    private static boolean isNearlyGrown(IWorld world, BlockPos pos) {
+        BlockState state = world.getBlockState(pos);
         if (ConfigHelper.blockHarvestAllowed(state.getBlock())) {
             if (state.getBlock() instanceof CropsBlock) {
                 CropsBlock crop = (CropsBlock) state.getBlock();
@@ -165,21 +153,49 @@ public class CropGrowthHandler {
         return false;
     }
 
-    public static class CropQueueEntry {
+    public static void scheduleCrop(IWorld world, BlockPos pos, int runsLeft) {
+        if (runsLeft <= 0) return;
+        int executeTick = ticks + HARVEST_DELAY;
+        queue.add(new CropQueueEntry(pos, world, executeTick, runsLeft));
+        if (data != null) data.markDirty();
+    }
+
+    public static class CropQueueEntry implements Comparable<CropQueueEntry> {
         private final BlockPos pos;
         private final IWorld world;
+        private final int tick;
+        private int count;
 
-        public CropQueueEntry(BlockPos pos, IWorld world) {
+        public BlockPos getPos() { return pos; }
+        public IWorld getWorld() { return world; }
+
+        public CropQueueEntry(BlockPos pos, IWorld world, int tick, int count) {
             this.pos = pos;
             this.world = world;
+            this.tick = tick;
+            this.count = count;
         }
 
-        public BlockPos getPos() {
-            return pos;
+        public void execute() {
+            if (world.isBlockLoaded(pos)) {
+                if (isFullyGrown(world, pos)) {
+                    EntityStrawGolem golem = getCropGolem(world, pos);
+                    if (golem != null) {
+                        golem.setHarvesting(pos);
+                        return;
+                    }
+                    scheduleCrop(world, pos, --count);
+                }
+            } else scheduleCrop(world, pos, count);
         }
 
-        public IWorld getWorld() {
-            return world;
+        @Override
+        public int compareTo(CropQueueEntry entry) {
+            if (entry.tick == tick) {
+                if (entry.count == count) return 0;
+                return entry.count > count ? 1 : -1;
+            }
+            return entry.tick > tick ? -1 : 1;
         }
     }
 }
