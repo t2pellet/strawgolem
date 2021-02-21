469538d557eed607f0efe04898dd78b52f8b2149
Tenzin Pelletier <pelletiertenzin2000@gmail.com>
2021-02-20 23:14:05 -0500

    Fix main entrypoint

--- src/main/java/com/commodorethrawn/strawgolem/Strawgolem.java
+++ src/main/java/com/commodorethrawn/strawgolem/Strawgolem.java
@@ -1,63 +1,60 @@
 package com.commodorethrawn.strawgolem;
 
 import com.commodorethrawn.strawgolem.client.compat.CompatHwyla;
-import com.commodorethrawn.strawgolem.client.renderer.entity.RenderIronGolem;
-import com.commodorethrawn.strawgolem.client.renderer.entity.RenderStrawGolem;
-import com.commodorethrawn.strawgolem.config.ConfigHelper;
-import com.commodorethrawn.strawgolem.config.ConfigHolder;
-import com.commodorethrawn.strawgolem.entity.capability.lifespan.ILifespan;
-import com.commodorethrawn.strawgolem.entity.capability.lifespan.Lifespan;
-import com.commodorethrawn.strawgolem.entity.capability.lifespan.LifespanStorage;
-import com.commodorethrawn.strawgolem.entity.capability.memory.IMemory;
-import com.commodorethrawn.strawgolem.entity.capability.memory.Memory;
-import com.commodorethrawn.strawgolem.entity.capability.memory.MemoryStorage;
-import com.commodorethrawn.strawgolem.entity.capability.profession.IProfession;
-import com.commodorethrawn.strawgolem.entity.capability.profession.Profession;
-import com.commodorethrawn.strawgolem.entity.capability.profession.ProfessionStorage;
+import com.commodorethrawn.strawgolem.events.*;
 import com.commodorethrawn.strawgolem.network.PacketHandler;
-import net.minecraft.entity.EntityType;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.capabilities.CapabilityManager;
-import net.minecraftforge.fml.ModList;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import com.commodorethrawn.strawgolem.storage.StrawgolemSaveData;
+import mcp.mobius.waila.api.event.WailaTooltipEvent;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(Strawgolem.MODID)
-public class Strawgolem {
+import java.io.IOException;
+
+public class Strawgolem implements ModInitializer, ClientModInitializer {
     public static final String MODID = "strawgolem";
     public static final Logger logger = LogManager.getLogger(MODID);
+    private static StrawgolemSaveData data;
 
-    public Strawgolem() {
-        logger.info("Initializing strawgolem");
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
-        if (ModList.get().isLoaded("waila")) {
-            MinecraftForge.EVENT_BUS.register(CompatHwyla.class);
+    @Override
+    public void onInitialize() {
+        ServerLifecycleEvents.SERVER_STARTING.register(server -> {
+            data = new StrawgolemSaveData(server);
+            try {
+                data.loadData();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        ServerLifecycleEvents.SERVER_STOPPING.register(server -> {
+            try {
+                data.saveData();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        ServerLifecycleEvents.SERVER_STOPPING.register(IronGolemHandler::stopHolding);
+        ServerTickEvents.END_WORLD_TICK.register(CropGrowthHandler::tick);
+        CropGrowthCallback.EVENT.register(CropGrowthHandler::onCropGrowth);
+        UseBlockCallback.EVENT.register(GolemCreationHandler::onGolemBuilt);
+        UseBlockCallback.EVENT.register(GolemCreationHandler::onGolemBuiltAlternate);
+        UseBlockCallback.EVENT.register(GolemChestHandler::setPriorityChest);
+        if (FabricLoader.getInstance().isModLoaded("waila")) {
+            WailaTooltipEvent.WAILA_HANDLE_TOOLTIP.register(CompatHwyla::patchTooltip);
         }
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ConfigHolder.COMMON_SPEC);
     }
 
-    public void commonSetup(FMLCommonSetupEvent event) {
-        logger.info("Strawgolem common setup");
-        CapabilityManager.INSTANCE.register(ILifespan.class, new LifespanStorage(), Lifespan::new);
-        CapabilityManager.INSTANCE.register(IMemory.class, new MemoryStorage(), Memory::new);
-        CapabilityManager.INSTANCE.register(IProfession.class, new ProfessionStorage(), Profession::new);
+    @Override
+    public void onInitializeClient() {
         PacketHandler.register();
     }
 
-    public void clientSetup(FMLClientSetupEvent event) {
-        logger.info("Strawgolem client setup");
-        RenderingRegistry.registerEntityRenderingHandler(Registry.STRAW_GOLEM_TYPE, RenderStrawGolem::new);
-        if (ConfigHelper.doGolemPickup()) {
-            RenderingRegistry.registerEntityRenderingHandler(EntityType.IRON_GOLEM, RenderIronGolem::new);
-        }
+    public static StrawgolemSaveData getSaveData() {
+        return data;
     }
-
 }
