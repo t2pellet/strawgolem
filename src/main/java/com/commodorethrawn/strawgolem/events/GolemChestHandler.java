b8538431eed15d13dd049c8afd0e4a1f57e74950
Tenzin Pelletier <pelletiertenzin2000@gmail.com>
2021-02-20 23:13:51 -0500

    Fix event handlers

---
+++ src/main/java/com/commodorethrawn/strawgolem/events/GolemChestHandler.java
@@ -0,0 +1,65 @@
+package com.commodorethrawn.strawgolem.events;
+
+import com.commodorethrawn.strawgolem.Strawgolem;
+import com.commodorethrawn.strawgolem.config.ConfigHelper;
+import com.commodorethrawn.strawgolem.entity.EntityStrawGolem;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.Items;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class GolemChestHandler {
+
+    private static final Map<UUID, Integer> playerToGolemMap = new HashMap<>();
+
+    private GolemChestHandler() {
+    }
+
+    public static void addMapping(UUID player, Integer golem) {
+        playerToGolemMap.put(player, golem);
+    }
+
+    /**
+     * Sets the chest that golem will always prioritize going to deliver
+     */
+    public static ActionResult setPriorityChest(PlayerEntity playerEntity, World world, Hand hand, BlockHitResult blockHitResult) {
+        if (!world.isClient) {
+            BlockEntity blockEntity = world.getBlockEntity(blockHitResult.getBlockPos());
+            if (blockEntity != null
+                && hand == Hand.MAIN_HAND
+                && playerEntity.getMainHandStack().getItem() == Items.WHEAT
+                && blockEntity instanceof Inventory
+                && playerToGolemMap.containsKey(playerEntity.getUuid())) {
+                EntityStrawGolem golem = (EntityStrawGolem) world.getEntityById(playerToGolemMap.get(playerEntity.getUuid()));
+                if (golem != null) {
+                    golem.getMemory().setPriorityChest(blockHitResult.getBlockPos());
+                    golem.getMemory().addPosition(world, blockHitResult.getBlockPos());
+                    Text text = new LiteralText(golem.getDisplayName().getString() + " will now deliver to this chest");
+                    playerEntity.sendMessage(text, true);
+                    playerToGolemMap.remove(playerEntity.getUuid());
+
+                    if (ConfigHelper.isTetherEnabled()) {
+                        BlockPos golemPos = golem.getBlockPos();
+                        BlockPos anchorPos = blockHitResult.getBlockPos();
+                        if (golemPos.getManhattanDistance(anchorPos) > ConfigHelper.getTetherMaxRange()) {
+                            Strawgolem.logger.debug(golem.getEntityId() + " setting new anchor " + anchorPos);
+                            golem.getMemory().setAnchorPos(anchorPos);
+                        }
+                    }
+                }
+            }
+        }
+        return ActionResult.PASS;
+    }
+}
--- src/main/java/com/commodorethrawn/strawgolem/events/IronGolemHandler.java
+++ src/main/java/com/commodorethrawn/strawgolem/events/IronGolemHandler.java
@@ -1,26 +1,23 @@
 package com.commodorethrawn.strawgolem.events;
 
-import com.commodorethrawn.strawgolem.Strawgolem;
-import com.commodorethrawn.strawgolem.config.ConfigHelper;
 import com.commodorethrawn.strawgolem.entity.ai.PickupGolemGoal;
-import net.minecraft.entity.passive.IronGolemEntity;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import com.commodorethrawn.strawgolem.mixin.GoalSelectorAccessor;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.GoalSelector;
+import net.minecraft.entity.ai.goal.PrioritizedGoal;
+import net.minecraft.server.MinecraftServer;
 
-@EventBusSubscriber(modid = Strawgolem.MODID, bus = EventBusSubscriber.Bus.FORGE)
 public class IronGolemHandler {
 
-    private IronGolemHandler() {
+    public static void stopHolding(MinecraftServer server) {
+        server.getWorlds().forEach(world -> {
+            world.getEntitiesByType(EntityType.IRON_GOLEM, e -> true).forEach(golem -> {
+                GoalSelector goalSelector = ((GoalSelectorAccessor) golem).goalSelector();
+                goalSelector.getRunningGoals()
+                        .filter(goal -> goal.getGoal() instanceof PickupGolemGoal)
+                        .forEach(PrioritizedGoal::stop);
+            });
+        });
     }
 
-    @SubscribeEvent
-    public static void ironGolemSpawn(EntityJoinWorldEvent event) {
-        if (!event.getWorld().isRemote
-                && event.getEntity() instanceof IronGolemEntity
-                && ConfigHelper.doGolemPickup()) {
-            IronGolemEntity golem = (IronGolemEntity) event.getEntity();
-            golem.goalSelector.addGoal(2, new PickupGolemGoal(golem, 0.8D));
-        }
-    }
 }
