f1871c66013d838e4eb439b72693b431cfbeb020
Tenzin Pelletier <tpelletier@paramountcommerce.com>
2021-02-20 23:13:44 -0500

    Fix crop system

--- src/main/java/com/commodorethrawn/strawgolem/storage/StrawgolemSaveData.java
+++ src/main/java/com/commodorethrawn/strawgolem/storage/StrawgolemSaveData.java
@@ -1,77 +1,74 @@
 package com.commodorethrawn.strawgolem.storage;
 
-import com.commodorethrawn.strawgolem.Strawgolem;
 import com.commodorethrawn.strawgolem.events.CropGrowthHandler;
 import com.mojang.serialization.Dynamic;
+import net.minecraft.client.render.SkyProperties;
 import net.minecraft.nbt.*;
-import net.minecraft.util.RegistryKey;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.WorldSavePath;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.DimensionType;
+import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
-import net.minecraft.world.server.ServerWorld;
-import net.minecraft.world.storage.DimensionSavedDataManager;
-import net.minecraft.world.storage.WorldSavedData;
-import net.minecraftforge.common.util.Constants;
-import net.minecraftforge.fml.server.ServerLifecycleHooks;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.level.storage.LevelStorage.Session;
 
-import javax.annotation.Nonnull;
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 
-public class StrawgolemSaveData extends WorldSavedData {
+public class StrawgolemSaveData {
 
-    public StrawgolemSaveData() {
-        super(Strawgolem.MODID);
-    }
+    private final File worldDataDir;
+    private final MinecraftServer server;
+    private static final String GOLEM_DATA_NAME = "strawgolem.dat";
+    private static final int TAG_COMPOUND = 10;
 
-    public StrawgolemSaveData(String name) {
-        super(name);
-        markDirty();
-    }
-
-    public static StrawgolemSaveData get(final ServerWorld world) {
-        DimensionSavedDataManager storage = world.getSavedData();
-        return storage.getOrCreate(StrawgolemSaveData::new, Strawgolem.MODID);
+    public StrawgolemSaveData(MinecraftServer server) {
+        this.server = server;
+        String url = server.getSavePath(WorldSavePath.LEVEL_DAT).toFile().getParentFile().getAbsolutePath();
+        worldDataDir = new File(url + "/data");
     }
 
     private static final String WORLD = "world";
     private static final String POS = "pos";
 
-    @Override
-    public void read(CompoundNBT nbt) {
-        ListNBT listTag = nbt.getList("listTag", Constants.NBT.TAG_COMPOUND);
-        for (INBT tag : listTag) {
-            CompoundNBT entryTag = (CompoundNBT) tag;
-            BlockPos pos = null;
-            World world = null;
-            if (entryTag.get(POS) != null) {
-                pos = NBTUtil.readBlockPos(entryTag.getCompound(POS));
-            }
-            if (entryTag.get(WORLD) != null) {
-                RegistryKey<World> dim = DimensionType.func_236025_a_(new Dynamic<>(NBTDynamicOps.INSTANCE, entryTag.get(WORLD)))
-                        .result().orElseThrow(() -> new IllegalArgumentException("Invalid map dimension: " + entryTag.get(WORLD)));
-                if (dim != null) world = ServerLifecycleHooks.getCurrentServer().getWorld(dim);
-            }
-            if (world != null && pos != null) {
-                CropGrowthHandler.scheduleCrop(world, pos, 3);
-            }
+    public void loadData() throws IOException {
+        File saveFile = new File(worldDataDir, GOLEM_DATA_NAME);
+        if (saveFile.exists() && saveFile.isFile()) {
+            CompoundTag nbt = NbtIo.readCompressed(saveFile);
+            ListTag listTag = nbt.getList("listTag", TAG_COMPOUND);
+            listTag.forEach(tag -> {
+                CompoundTag entryTag = (CompoundTag) tag;
+                if (entryTag.get(WORLD) != null) {
+                    RegistryKey<World> dim = DimensionType.method_28521(new Dynamic<>(NbtOps.INSTANCE, entryTag.get(WORLD)))
+                            .result().orElseThrow(() -> new IllegalArgumentException("Invalid map dimension: " + entryTag.get(WORLD)));
+                    if (entryTag.get(POS) != null && dim != null) {
+                        BlockPos pos = NbtHelper.toBlockPos((CompoundTag) entryTag.get(POS));
+                        World world = server.getWorld(dim);
+                        if (world != null) {
+                            CropGrowthHandler.scheduleCrop(world, pos, 3);
+                        }
+                    }
+                }
+            });
         }
     }
 
-    @Nonnull
-    @Override
-    public CompoundNBT write(@Nonnull CompoundNBT compound) {
+    public void saveData() throws IOException {
+        CompoundTag compound = new CompoundTag();
         Iterator<CropGrowthHandler.CropQueueEntry> cropIterator = CropGrowthHandler.getCrops();
-        ListNBT listTag = new ListNBT();
+        ListTag listTag = new ListTag();
         while (cropIterator.hasNext()) {
-            CompoundNBT entryTag = new CompoundNBT();
+            CompoundTag entryTag = new CompoundTag();
             CropGrowthHandler.CropQueueEntry entry = cropIterator.next();
-            entryTag.put(POS, NBTUtil.writeBlockPos(entry.getPos()));
-            ResourceLocation.field_240908_a_.encodeStart(NBTDynamicOps.INSTANCE, entry.getWorld().func_234923_W_().func_240901_a_())
+            entryTag.put(POS, NbtHelper.fromBlockPos(entry.getPos()));
+            Identifier.CODEC.encodeStart(NbtOps.INSTANCE, entry.getWorld().getRegistryKey().getValue())
                     .result().ifPresent(dim -> entryTag.put(WORLD, dim));
             listTag.add(entryTag);
         }
         compound.put("listTag", listTag);
-        return compound;
+        File file = new File(worldDataDir, GOLEM_DATA_NAME);
+        NbtIo.writeCompressed(compound, file);
     }
 }
