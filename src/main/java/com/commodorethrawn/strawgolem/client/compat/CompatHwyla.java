c0261c88f92ab19fb0bb0956e66f853ad13de1a1
Tenzin Pelletier <tpelletier@paramountcommerce.com>
2021-02-20 23:14:12 -0500

    HWYLA Compat

--- src/main/java/com/commodorethrawn/strawgolem/client/compat/CompatHwyla.java
+++ src/main/java/com/commodorethrawn/strawgolem/client/compat/CompatHwyla.java
@@ -3,25 +3,20 @@ package com.commodorethrawn.strawgolem.client.compat;
 import com.commodorethrawn.strawgolem.config.ConfigHelper;
 import com.commodorethrawn.strawgolem.entity.EntityStrawGolem;
 import mcp.mobius.waila.api.event.WailaTooltipEvent;
-import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraft.text.TranslatableText;
 
 public class CompatHwyla {
 
-    private CompatHwyla() {
-    }
-
-    @SubscribeEvent
-    public static void onTooltip(WailaTooltipEvent event) {
+    public static void patchTooltip(WailaTooltipEvent event) {
         if (event.getAccessor().getEntity() instanceof EntityStrawGolem && ConfigHelper.isEnableHwyla()) {
             EntityStrawGolem golem = (EntityStrawGolem) event.getAccessor().getEntity();
-            float daysLeft = golem.getCurrentLifespan() / 24000F;
+            float daysLeft = golem.getLifespan().get() / 24000F;
             if (daysLeft >= 1) {
-                event.getCurrentTip().add(new TranslationTextComponent("strawgolem.lifespan", Math.round(daysLeft)));
-            } else if (golem.getCurrentLifespan() < 0) {
-                event.getCurrentTip().add(new TranslationTextComponent("strawgolem.lifespan", '\u221e'));
+                event.getCurrentTip().add(new TranslatableText("strawgolem.lifespan", Math.round(daysLeft)));
+            } else if (golem.getLifespan().get() < 0) {
+                event.getCurrentTip().add(new TranslatableText("strawgolem.lifespan", '\u221e'));
             } else {
-                event.getCurrentTip().add(new TranslationTextComponent("strawgolem.lifespan", "<1"));
+                event.getCurrentTip().add(new TranslatableText("strawgolem.lifespan", "<1"));
             }
         }
     }
