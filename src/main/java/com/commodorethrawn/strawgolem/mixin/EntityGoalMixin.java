adf640658e5f959d53b6bcccdf7466a4e0a000cd
Tenzin Pelletier <pelletiertenzin2000@gmail.com>
2021-02-22 19:34:34 -0500

    Raiders attack straw golems

--- src/main/java/com/commodorethrawn/strawgolem/mixin/EntityGoalMixin.java
+++ src/main/java/com/commodorethrawn/strawgolem/mixin/EntityGoalMixin.java
@@ -1,38 +1,44 @@
 package com.commodorethrawn.strawgolem.mixin;
 
+import com.commodorethrawn.strawgolem.entity.EntityStrawGolem;
 import com.commodorethrawn.strawgolem.entity.ai.PickupGolemGoal;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.ai.goal.GoalSelector;
-import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.ai.goal.FollowTargetGoal;
 import net.minecraft.entity.passive.IronGolemEntity;
+import net.minecraft.entity.raid.RaiderEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.world.World;
-import org.lwjgl.system.CallbackI;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerWorld.class)
-public class IronGolemMixin {
+public class EntityGoalMixin {
 
     @Inject(method = "addEntity", at = @At("TAIL"))
-    public void patchIronGolem1(Entity entity, CallbackInfoReturnable<?> info) {
+    public void patchNewEntity(Entity entity, CallbackInfoReturnable<?> info) {
         patchIronGolem(entity);
+        patchRaider(entity);
     }
 
     @Inject(method = "loadEntityUnchecked", at = @At("TAIL"))
-    public void patchIronGolem2(Entity entity, CallbackInfo info) {
+    public void patchOldEntity(Entity entity, CallbackInfo info) {
         patchIronGolem(entity);
+        patchRaider(entity);
     }
 
     public void patchIronGolem(Entity entity) {
         if (entity instanceof IronGolemEntity) {
             IronGolemEntity ironGolem = (IronGolemEntity) entity;
             ((GoalSelectorAccessor) ironGolem).goalSelector().add(2, new PickupGolemGoal(ironGolem, 0.8D));
+        }
+    }
+
+    public void patchRaider(Entity entity) {
+        if (entity instanceof RaiderEntity) {
+            RaiderEntity raider = (RaiderEntity) entity;
+            ((TargetSelectorAccessor) raider).targetSelector().add(2, new FollowTargetGoal<>(raider, EntityStrawGolem.class, true));
         }
     }
 
