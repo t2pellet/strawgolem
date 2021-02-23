7cb77dd292604b14437661df73a0cc9a27d52e2e
Tenzin Pelletier <tpelletier@paramountcommerce.com>
2021-02-22 20:00:25 -0500

    Animals munch golems sometimes

--- src/main/java/com/commodorethrawn/strawgolem/mixin/EntityGoalMixin.java
+++ src/main/java/com/commodorethrawn/strawgolem/mixin/EntityGoalMixin.java
@@ -1,9 +1,11 @@
 package com.commodorethrawn.strawgolem.mixin;
 
 import com.commodorethrawn.strawgolem.entity.EntityStrawGolem;
+import com.commodorethrawn.strawgolem.entity.ai.MunchGolemGoal;
 import com.commodorethrawn.strawgolem.entity.ai.PickupGolemGoal;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ai.goal.FollowTargetGoal;
+import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.IronGolemEntity;
 import net.minecraft.entity.raid.RaiderEntity;
 import net.minecraft.server.world.ServerWorld;
@@ -20,12 +22,14 @@ public class EntityGoalMixin {
     public void patchNewEntity(Entity entity, CallbackInfoReturnable<?> info) {
         patchIronGolem(entity);
         patchRaider(entity);
+        patchAnimal(entity);
     }
 
     @Inject(method = "loadEntityUnchecked", at = @At("TAIL"))
     public void patchOldEntity(Entity entity, CallbackInfo info) {
         patchIronGolem(entity);
         patchRaider(entity);
+        patchAnimal(entity);
     }
 
     public void patchIronGolem(Entity entity) {
@@ -39,6 +43,13 @@ public class EntityGoalMixin {
         if (entity instanceof RaiderEntity) {
             RaiderEntity raider = (RaiderEntity) entity;
             ((TargetSelectorAccessor) raider).targetSelector().add(2, new FollowTargetGoal<>(raider, EntityStrawGolem.class, true));
+        }
+    }
+
+    public void patchAnimal(Entity entity) {
+        if (entity instanceof AnimalEntity) {
+            AnimalEntity animal = (AnimalEntity) entity;
+            ((GoalSelectorAccessor) animal).goalSelector().add(2, new MunchGolemGoal(animal, 0.8D));
         }
     }
 
