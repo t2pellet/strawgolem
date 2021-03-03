3056cbe331d2ef9a0624a5331f94bd5bb57dda83
Tenzin Pelletier <pelletiertenzin2000@gmail.com>
2021-03-02 23:08:20 -0500

    Added strawng golem creation (via lightning)

--- src/main/java/com/commodorethrawn/strawgolem/events/GolemCreationHandler.java
+++ src/main/java/com/commodorethrawn/strawgolem/events/GolemCreationHandler.java
@@ -1,6 +1,7 @@
 package com.commodorethrawn.strawgolem.events;
 
 import com.commodorethrawn.strawgolem.entity.EntityStrawGolem;
+import com.commodorethrawn.strawgolem.entity.EntityStrawngGolem;
 import com.commodorethrawn.strawgolem.registry.StrawgolemEntities;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -13,6 +14,8 @@ import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.StringVisitable;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -37,10 +40,10 @@ public class GolemCreationHandler {
             BlockPos placementPos = pos.add(direction.getX(), direction.getY(), direction.getZ());
             if (heldBlock == Blocks.CARVED_PUMPKIN) {
                 BlockPos hayPos = placementPos.down();
-                if (worldIn.getBlockState(hayPos).getBlock() == Blocks.HAY_BLOCK) spawnGolem(worldIn, hayPos, placementPos, result.getSide());
+                if (worldIn.getBlockState(hayPos).getBlock() == Blocks.HAY_BLOCK) spawnStrawGolem(worldIn, hayPos, placementPos, result.getSide());
             } else if (heldBlock == Blocks.HAY_BLOCK) {
                 BlockPos pumpkinPos = placementPos.up();
-                if (worldIn.getBlockState(pumpkinPos).getBlock() == Blocks.CARVED_PUMPKIN) spawnGolem(worldIn, placementPos, pumpkinPos, result.getSide());
+                if (worldIn.getBlockState(pumpkinPos).getBlock() == Blocks.CARVED_PUMPKIN) spawnStrawGolem(worldIn, placementPos, pumpkinPos, result.getSide());
             }
         }
         return ActionResult.PASS;
@@ -58,19 +61,20 @@ public class GolemCreationHandler {
             worldIn.setBlockState(result.getBlockPos(), Blocks.CARVED_PUMPKIN.getDefaultState().with(HorizontalFacingBlock.FACING, facing));
             worldIn.playSound(pos.getX(), pos.getY(), pos.getZ(), SoundEvents.BLOCK_PUMPKIN_CARVE, SoundCategory.BLOCKS, 1.0F, 1.0F, true);
             player.getMainHandStack().damage(1, player, p -> p.sendToolBreakStatus(Hand.MAIN_HAND));
-            return spawnGolem(worldIn, pos.down(), pos, result.getSide());
+            return spawnStrawGolem(worldIn, pos.down(), pos, result.getSide());
         }
         return ActionResult.PASS;
     }
 
     /**
-     * Spawns the strawgolem in the given world if theres a hay block at pos hay and pumpkin block at pos pumpkin
-     *
+     * Spawns the strawgolem in the given world
      * @param worldIn the world
      * @param hay     position of hay
      * @param pumpkin position of pumpkin
+     * @param facing the facing direction for the golem
+     * @return the ActionResult
      */
-    private static ActionResult spawnGolem(World worldIn, BlockPos hay, BlockPos pumpkin, Direction facing) {
+    private static ActionResult spawnStrawGolem(World worldIn, BlockPos hay, BlockPos pumpkin, Direction facing) {
         if (!worldIn.isClient) {
             ServerWorld world = (ServerWorld) worldIn;
             world.setBlockState(pumpkin, Blocks.AIR.getDefaultState());
@@ -78,6 +82,27 @@ public class GolemCreationHandler {
             EntityStrawGolem strawGolem = StrawgolemEntities.strawGolemEntityType().create(world);
             strawGolem.refreshPositionAndAngles(hay, facing.getHorizontal(), 0.0F);
             world.spawnEntity(strawGolem);
+            return ActionResult.SUCCESS;
+        }
+        return ActionResult.PASS;
+    }
+
+    /**
+     * Spawns the strawng golem in the given world, replacing the straw golem
+     * @param worldIn the world
+     * @param golem the straw golem
+     * @return the ActionReuslt
+     */
+    public static ActionResult spawnStrawngGolem(World worldIn, EntityStrawGolem golem) {
+        if (!worldIn.isClient) {
+            EntityStrawngGolem strawngGolem = StrawgolemEntities.strawngGolemEntityType().create(worldIn);
+            if (golem.hasCustomName()) strawngGolem.setCustomName(golem.getCustomName());
+            strawngGolem.refreshPositionAndAngles(golem.getBlockPos(), golem.headYaw, (float) golem.getLookControl().getLookY());
+            golem.remove();
+            if (worldIn.getBlockState(golem.getBlockPos()).getBlock() == Blocks.FIRE) {
+                worldIn.setBlockState(golem.getBlockPos(), Blocks.AIR.getDefaultState());
+            }
+            worldIn.spawnEntity(strawngGolem);
             return ActionResult.SUCCESS;
         }
         return ActionResult.PASS;
