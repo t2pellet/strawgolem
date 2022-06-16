8167db4314505e144c2c29383664fba0936798a5
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2022-06-16 19:40:17 -0400

    fix hat pivot point

--- Common/src/main/java/com/t2pellet/strawgolem/client/renderer/entity/model/ModelStrawGolem.java
+++ Common/src/main/java/com/t2pellet/strawgolem/client/renderer/entity/model/ModelStrawGolem.java
@@ -8,10 +8,7 @@ import net.minecraft.client.model.ArmedModel;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
-import net.minecraft.client.model.geom.builders.CubeListBuilder;
-import net.minecraft.client.model.geom.builders.LayerDefinition;
-import net.minecraft.client.model.geom.builders.MeshDefinition;
-import net.minecraft.client.model.geom.builders.PartDefinition;
+import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.HumanoidArm;
 
@@ -53,9 +50,9 @@ public class ModelStrawGolem extends EntityModel<EntityStrawGolem> implements Ar
 
         modelPartData.addOrReplaceChild("hat",
                 CubeListBuilder.create()
-                        .texOffs(0, 0).addBox(-5.0F, 0.0F, -5.0F, 10.0F, 0.0F, 10.0F)
-                        .texOffs(0, 10).addBox(-3.0F, -2.0F, -3.0F, 6.0F, 2.0F, 6.0F),
-                PartPose.offset(0.0F, 8.5F, 0.0F));
+                        .texOffs(0, 0).addBox(-5.0F, -2.0F, -5.0F, 10.0F, 0.0F, 10.0F, new CubeDeformation(0.0F))
+                        .texOffs(0, 10).addBox(-3.0F, -4.0F, -3.0F, 6.0F, 2.0F, 6.0F, new CubeDeformation(0.0F)),
+                PartPose.offset(0.0F, 10.5F, 0.0F));
         modelPartData.addOrReplaceChild("head",
                 CubeListBuilder.create()
                         .texOffs(26, 24).addBox(-2.0F, -4.0F, -2.0F, 4.0F, 4.0F, 4.0F)
