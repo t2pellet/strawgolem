6b74221de8824d1eb349dd4ca1ba4a6f820d09cb
t2pellet <t2pellet@uwaterloo.ca>
2023-09-28 17:51:50 -0400

    feat: drop item when panicked

--- common/src/main/java/com/t2pellet/strawgolem/entity/goals/golem/GolemPanicGoal.java
+++ common/src/main/java/com/t2pellet/strawgolem/entity/goals/golem/GolemPanicGoal.java
@@ -3,10 +3,16 @@ package com.t2pellet.strawgolem.entity.goals.golem;
 import com.t2pellet.strawgolem.StrawgolemConfig;
 import com.t2pellet.strawgolem.entity.StrawGolem;
 import net.minecraft.world.entity.ai.goal.PanicGoal;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
 
 public class GolemPanicGoal extends PanicGoal {
+
+    private final StrawGolem golem;
+
     public GolemPanicGoal(StrawGolem golem) {
         super(golem, 0.8D);
+        this.golem = golem;
     }
 
     @Override
@@ -15,5 +21,15 @@ public class GolemPanicGoal extends PanicGoal {
             return super.canUse();
         }
         return false;
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        // Drops item in hand
+        ItemStack heldItem = golem.getHeldItem().get();
+        golem.getHeldItem().set(ItemStack.EMPTY);
+        ItemEntity itemEntity = new ItemEntity(golem.level, golem.getX(), golem.getY() + 1, golem.getZ(), heldItem);
+        golem.level.addFreshEntity(itemEntity);
     }
 }
